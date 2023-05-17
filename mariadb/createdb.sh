928fe8217878244c3d2fe05d389e5f85a8e76109
Navoos <iacopocoram@gmail.com>
2023-05-17 15:41:26 +0100

    new structure

--- mariadb/createdb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mysqld &
-while ! mysqladmin ping -hlocalhost --silent; do
-	sleep 1
-done
-mysql -u root < /docker-entrypoint-initdb.d/init.sql
-wait
--- mariadb/init.sql
+++
@@ -1,6 +0,0 @@
-create database Wordpress;
-
-create user 'navoos'@'%';
-
-grant all privileges on Wordpress.* to 'navoos'@'%';
-flush privileges;
