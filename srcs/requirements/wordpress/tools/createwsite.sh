3cf32c346b1be4abcca380aa9b7087a7fef3a15f
Navoos <iacopocoram@gmail.com>
2023-05-20 15:49:46 +0100

    now the mariadb container and the wordpress container work in harmony

--- srcs/requirements/wordpress/tools/createwsite.sh
+++ srcs/requirements/wordpress/tools/createwsite.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-mkdir -p /var/www/Inception-website
-cd /var/www/Inception-website
-wp core download --allow-root
-wp core config --dbname=$DB_NAME --dbhost=$DB_HOST --dbuser=$USER --dbpass=$USER_PASSWORD --allow-root
+if [ ! -d /var/www/Inception-website ]; then
+	mkdir /var/www/Inception-website
+fi
 
-wp core install --url=https://yakhoudr.42.fr --title=Inception --admin=$WORD_ADMIN --admin_password=$WORD_ADMIN_PASSWORD --admin_email=$WORD_ADMIN_EMAIL --allow-root
+if [ ! -f /var/www/Inception-website/wp-config.php ]; then
+	wp core download --path=/var/www/Inception-website --allow-root
+	wp core config --path=/var/www/Inception-website --dbname=$DB_NAME --dbhost=$DB_HOST --dbuser=$USER --dbpass=$USER_PASSWORD --allow-root
+
+	wp core install --path=/var/www/Inception-website --url=https://yakhoudr.42.fr --title=Inception --admin_user=$WORD_ADMIN --admin_password=$WORD_ADMIN_PASSWORD --admin_email=$WORD_ADMIN_EMAIL --allow-root
+else
+	echo "Wordpress is already installed."
+fi
