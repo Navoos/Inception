84f36312577e3a76efa0b35c667c9861587f7768
Navoos <iacopocoram@gmail.com>
2023-05-31 16:41:49 +0100

    portainer

--- srcs/requirements/bonus/portainer/tools/launch.sh
+++ srcs/requirements/bonus/portainer/tools/launch.sh
@@ -3,10 +3,11 @@
 if [ ! -d /opt/portainer ]; then
 	mkdir /tmp/portainer
 	cd /tmp/portainer
-	wget https://github.com/portainer/portainer/releases/download/2.18.3/portainer-2.18.3-linux-amd64.tar.gz
+	wget -q https://github.com/portainer/portainer/releases/download/2.18.3/portainer-2.18.3-linux-amd64.tar.gz
 	tar -zxvf portainer-2.18.3-linux-amd64.tar.gz
 	mv portainer /opt/portainer
 	rm -f portainer-2.18.3-linux-amd64.tar.gz
 fi
 
+echo "portainer is now running."
 exec /opt/portainer/portainer
